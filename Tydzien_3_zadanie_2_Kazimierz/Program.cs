e3ef87926f7eb2af9103b214a06c39e6fb451fe9
Hubert Bąk <hobo-11@o2.pl>
2022-01-12 22:27:20 +0100

    Dodaj pliki projektów.

---
+++ Tydzien_3_zadanie_2_Kazimierz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien_3_zadanie_2_Kazimierz
+{
+    class Program
+    {
+        private static int GetNumber()
+        {
+            while (true)
+            {  
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową liczbę, Spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(0, 101);
+            Console.WriteLine("Właśnie wylosowałem 1 liczbę z zakresu 0 - 100.Spróbuj ją odgadnąć.");
+            var numberOfTries = 0;
+            while(true){
+                numberOfTries++;
+                Console.WriteLine("\nPodaj Twoją liczbę:");
+                var userNumber = GetNumber();
+                if(userNumber < randomNumber){
+                    Console.WriteLine("Twoja liczba jest mniejsza niż wylosowana.Spróbuj ponownie.");
+                    continue;
+                }
+                if(userNumber > randomNumber){
+                    Console.WriteLine("Twoja liczba jest większa niż wylosowana.Spróbuj ponownie.");
+                    continue;
+                }
+                Console.WriteLine($"Super! Odgadłeś wylosowaną liczbę w { numberOfTries} próbach");
+                break;
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Zadanie_domowe_2_tydzie3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_domowe_2_tydzie3
+{
+    class Program
+    {
+        //private static int GetRandomNumber()
+        //{
+        //    bool test = true;
+        //    int number;
+        //    do
+        //    {
+        //        test = int.TryParse(Console.ReadLine(), out int retInt);
+        //        if (test)
+        //        {
+        //            return retInt;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Błędne wprowadzenie!");
+        //        }
+        //    } while (!test);
+        //}
+
+        private static int GetIntNumber()
+        {
+            for (;;)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int numberToReturn))
+                {
+                    Console.WriteLine("Niepoprawne wprowadzenie, wprowadź liczbę całkowitą z zakresu 0 - 100!");
+                }
+                return numberToReturn;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var randomNumber = rnd.Next(0, 101);  //losowa liczba z zakresu 0 - 100
+            var shouldStop = false;
+            int numberOfTrials = 0;
+            Console.WriteLine("Wylosowana została losowa liczba od 0 do 100, spróbuj odgadnąć!");
+            while (!shouldStop)
+            {
+                numberOfTrials++;
+                Console.WriteLine("Próba numer: " + numberOfTrials);
+                Console.WriteLine("Wprowadź liczbę z zakresu od 0 do 100: ");
+                var typedNumber = GetIntNumber();
+
+                
+                if (typedNumber == randomNumber)
+                {
+                    shouldStop = true;
+                    Console.WriteLine("Odgadłeś liczbę!");
+                }else if (randomNumber > typedNumber)
+                {
+                    Console.WriteLine("Wpisałeś liczbę o zbyt małej wartości");
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś liczbę o zby wielkiej wartości");
+                }
+                
+            }
+
+            Console.WriteLine("Gratulacje! Liczba odgadnięta! Liczba prób: " + numberOfTrials);
+            Console.ReadLine();
+        }
+    }
+}
